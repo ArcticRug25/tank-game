48547f7d648325a3bec9100e7fc7ba2cbe413aaa
ArcticRug25 <wyw82839148@163.com>
2022-06-04 21:29:41 +0800

    添加eslint配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
 const el = document.querySelector<HTMLCanvasElement>('#canvas')!
 const app = el.getContext('2d')!
 
-app.fillStyle = '#000'
-app.fillRect(0, 0, 300, 300)
+// app.fillStyle = '#000'
+// app.fillRect(0, 0, 300, 300)
 
-app.fillStyle = '#f1c40f'
-app.fillRect(el.width / 2 - 50, el.height / 2 - 50, 100, 100)
+// app.fillStyle = '#f1c40f'
+// app.fillRect(el.width / 2 - 50, el.height / 2 - 50, 100, 100)
+
+// 绘制线条
+// app.strokeStyle = '#8e44ad'
+// app.lineWidth = 30
+// app.lineJoin = 'round'
+// app.strokeRect(50, 50, 200, 200)
+
+// 绘制圆形
+// // app.fillStyle = '#8e44ad'
+// app.strokeStyle = 'red'
+// app.lineWidth = 20
+// app.arc(100, 100, 50, 0, 2 * Math.PI)
+// app.stroke()
+
+// 绘制多边形
+// app.beginPath()
+// app.moveTo(el.width / 2, 10)
+// app.lineTo(el.width, 250)
+// app.lineTo(10, 250)
+// app.closePath()
+// app.strokeStyle = '#e74c3c'
+// app.lineWidth = 2
+// app.stroke()
+
+// 渐变色
+// const gradient = app.createLinearGradient(0, 0, 300, 300)
+// gradient.addColorStop(0, '#16a085')
+// gradient.addColorStop(0.5, '#e67e22')
+// gradient.addColorStop(1, '#9b59b6')
+// app.strokeStyle = gradient
+// app.lineWidth = 50
+// app.lineJoin = 'round'
+// app.stroke()
+// app.strokeRect(50, 50, 200, 200)
+
+// 文字处理
+// app.fillStyle = '#34495e'
+// app.fillRect(0, 0, el.width, el.height)
+// app.font = '60px SourceHanSansSC-Normal'
+// // app.fillStyle = 'white'
+// app.strokeStyle = 'white'
+// app.lineWidth = 2
+// app.textBaseline = 'top'
+// app.strokeText('Tank-Game', 50, 0)
+
+// 图片贴图的使用
+
