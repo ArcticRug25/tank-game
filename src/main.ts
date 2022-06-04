3598f61c2fb016d734f0cf7f38e4c1650e9d4a45
ArcticRug25 <wyw82839148@163.com>
2022-06-04 14:24:59 +0800

    init canvas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './style.css'
+const el = document.querySelector<HTMLCanvasElement>('#canvas')!
+const app = el.getContext('2d')!
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+app.fillStyle = '#000'
+app.fillRect(0, 0, 300, 300)
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+app.fillStyle = '#f1c40f'
+app.fillRect(el.width / 2 - 50, el.height / 2 - 50, 100, 100)
