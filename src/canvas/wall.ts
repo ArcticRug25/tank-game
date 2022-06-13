ad8ac6c3e4326a73ac3c5fbfb6798403fd44aa08
ArcticRug25 <wyw82839148@163.com>
2022-06-13 19:10:59 +0800

    chore: add boss wall

--- src/canvas/wall.ts
+++ src/canvas/wall.ts
@@ -13,6 +13,30 @@ export default new class Wall extends CanvasAbstract implements ICanvas {
 
   render(): void {
     super.createModels()
+    this.createBossWall()
     super.renderModels()
+  }
+
+  createBossWall() {
+    const cw = config.canvas.width
+    const ch = config.canvas.height
+    const mw = config.model.width
+    const mh = config.model.height
+    const pos = [
+      { x: cw / 2 - mw * 2, y: ch - mh },
+      { x: cw / 2 - mw * 2, y: ch - mh * 2 },
+      { x: cw / 2 - mw * 2, y: ch - mh * 3 },
+      { x: cw / 2, y: ch - mh * 3 },
+      { x: cw / 2 - mw, y: ch - mh * 3 },
+      { x: cw / 2 + mw, y: ch - mh * 3 },
+      { x: cw / 2 + mw * 2, y: ch - mh * 3 },
+      { x: cw / 2 + mw * 2, y: ch - mh * 2 },
+      { x: cw / 2 + mw * 2, y: ch - mh },
+    ]
+    pos.forEach((position) => {
+      const WallModel = this.Model() as ModelConstructor
+      const instance = new WallModel(position.x, position.y)
+      this.models.push(instance)
+    })
   }
 }('wall')
