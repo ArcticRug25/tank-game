57d5d37a0af01c4eb975358feff785fc4f5b2bce
ArcticRug25 <wyw82839148@163.com>
2022-06-15 15:09:04 +0800

    chore: finished

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import tank from './canvas/tank'
 import bullet from './canvas/bullet'
 import boss from './canvas/boss'
 import player from './canvas/player'
+import audio from './service/audio'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.style.width = `${config.canvas.width}px`
@@ -35,6 +36,7 @@ export default {
     if (this.isStart)
       return
 
+    audio.start()
     this.isStart = true
     app.style.backgroundImage = 'none'
     await Promise.all(imgPromises)
@@ -52,5 +54,22 @@ export default {
     clearInterval(this.interval)
     tank.stop()
     bullet.stop()
+    this.text()
+  },
+  text() {
+    const el = document.createElement('canvas')
+    el.width = config.canvas.width
+    el.height = config.canvas.height
+    const ctx = el.getContext('2d')!
+    ctx.fillStyle = 'red'
+    ctx.font = '80px CascadiaMono'
+    ctx.textBaseline = 'top'
+    ctx.textAlign = 'center'
+    ctx.fillText(
+      this.state === 1 ? '游戏胜利' : '游戏失败',
+      config.canvas.width / 2,
+      config.canvas.height / 2,
+    )
+    app.appendChild(el)
   },
 }
